76a1fc294d00b2967e0da964e5205ad30430b55d
MacBookAir <macbookair@MacBookAirs-MacBook-Air-2.local>
2022-07-06 22:06:30 +0200

    Add styles for multiple sections

---
+++ script.js
@@ -0,0 +1,9 @@
+// const parallax = document.getElementById("banner");
+// window.addEventListener("scroll", function() {
+//   let offset = window.pageYOffset;
+//   parallax.style.backgroundPositionY = offset * 0.8 + "px";
+// })
+
+// var image = document.getElementsByClassName('banner__img');
+// new simpleParallax(image);
+
